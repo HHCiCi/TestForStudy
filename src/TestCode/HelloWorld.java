365a55b095cc081e997c6e6c234c26f1df35f29b
cici <lotus-hehuan@163.com>
2019-12-20 19:28:27 +0800

    commit to where?

--- src/TestCode/HelloWorld.java
+++ src/TestCode/HelloWorld.java
@@ -3,6 +3,15 @@ package TestCode;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Helloworld");
+		
+	}
+	
+	public void add(){
+		System.out.println("check what i am add");
+	}
+	
+	public void delete(){
+		System.out.println("guess if i am ");
 	}
 
 }
